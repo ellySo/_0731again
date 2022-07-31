fcf5039c1d1d60fa75a9128b324786a8deee2ce1
ellyso <skyoung0701@gmail.com>
2022-07-31 17:05:55 +0900

    상단 모바일 서버메뉴노출미완

--- js/base.js
+++ js/base.js
@@ -2,7 +2,7 @@
 // $(document).ready(function(){
     var navidata = [
         {
-            d1 : ['회사소개','company.html'],
+            d1 : ['회사소개','#about'],
             d2 : [
 
                 ['비전','v.html'],
@@ -36,7 +36,7 @@
             }
     ]
 
-    // console.log('a태그안의 값',navidata[1].d2[0][0],'href 깅크값',navidata[1].d2[0][1])
+    // console.log('a태그안의 값',navidata[1].d2[0][0],'href 링크값',navidata[1].d2[0][1])
     
 
 // })
@@ -45,17 +45,18 @@ $(document).ready(function(){
     var navitag = "";
 
     for(var i in navidata){
-        navitag +="<li><a href='"+navidata[i].d1[1]+"'>'+navidata[i].d1[1]+'</a>"
-            for(var j in navidata[i].d2){
-             if(j == 0 ) navitag +="<ul>";
-             navitag +="<li><a href=''></a></li>";
-             if(j == navidata[i].d2.length - 1) navitag +="</ul>";
-            }
+        // 대메뉴출력
+        navitag +='<li class="nav-item position-relative"><a href='${navidata[i].d1[1]}' class="py=2 py-md-0 d-block px-0 px-md-3">${navidata[i].d1[0]}</a>'
+            for(var j in navidata[i].d2){ // j초, i분 j변수가 다 돌 때까지 i는 증가하질 못하고 대기
+                if( j == 0 ) navitag +=
+              navitag +='<ul>'; //첫번째 추가
 
-        navitag +="</li>"
+              navitag +='</ul>'; // 제일 마지막 끝나고 추가
+            }
+        navitag += '</li>'
     }
     
-    $('#navi').html(navitag)
-    
+    $('navbarNav > ul').html(navitag);
+        
 
 })
