30b2e3e44ae4202f34271dc9c319dc7d8ffee086
ellyso <skyoung0701@gmail.com>
2022-07-31 15:33:29 +0900

    네비게이션 태그출력완료

---
+++ js/for구문.js
@@ -0,0 +1,37 @@
+const navidata = [
+    {link_product_list:"/category/야구잠바/24/"
+    ,name:"야구잠바"
+    ,param:"?cate_no=24"
+    ,cate_no:24
+    ,design_page_url:"product/list.html"
+    },
+    {link_product_list:"/category/반팔티/25/"
+    ,name:"반팔티"
+    ,param:"?cate_no=25"
+    ,cate_no:25
+    ,design_page_url:"product/list.html"
+    },
+    {link_product_list:"/category/단체티/26/"
+    ,name:"단체티"
+    ,param:"?cate_no=26"
+    ,cate_no:26
+    ,design_page_url:"product/list.html"
+    }
+    ]
+
+// var navitext = "<a href=\""+navidata[0].design_page_url+"\"+navidata[0].name+"</a>"
+// ec6
+// let navitext = '<a href="${navidata[0].design_page_url}">
+                    // ${navidata[0].name}
+                // </a>'
+
+// for(var i =0; i < navidata.length; i++){ // navidata는 반드시 array
+//     // 저 array을 하나하나 처리할 때 발생하는 index를 저장할 변수 i
+//     if( i == 0 ) console.log( '--------------')
+//     console.log(navidata[i].name)
+//     if (i == navidata.length -1 ) console.log('--------------')
+
+$(document.ready(function))
+
+
+
